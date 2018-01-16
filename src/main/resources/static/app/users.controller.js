6a4cd777910ffcbc757a37d7b1e729553aa56788
mrValtiel <mrvaltiel.gmail.com>
2018-01-16 20:40:14 +0100

    addUser() missing

--- src/main/resources/static/app/users.controller.js
+++ src/main/resources/static/app/users.controller.js
@@ -7,7 +7,7 @@
 
     UsersController.$inject = ['$http'];
 
-    function UsersController($http){
+    function UsersController($http) {
         var vm = this;
         vm.users = [];
         vm.getAll = getAll;
@@ -17,38 +17,65 @@
 
         init();
 
-        function init(){
+        function init() {
             getAll();
         }
 
-        function addUser(){
+        /* function addUser(){
             var url = "/users/create";
             $http.post(url).then(function(response){
                 vm.users = response.data;
             });
-        }
+        } */
 
-        function getAll(){
+        function getAll() {
             var url = "/users/all";
             var usersPromise = $http.get(url);
-            usersPromise.then(function(response){
+            usersPromise.then(function (response) {
                 vm.users = response.data;
             });
         }
 
-        function getDeleted(){
+        function getDeleted() {
             var url = "/users/deleted";
             var usersPromise = $http.get(url);
-            usersPromise.then(function(response){
+            usersPromise.then(function (response) {
                 vm.users = response.data;
             });
         }
 
-        function deleteUser(id){
+        function deleteUser(id) {
             var url = "/users/remove/" + id;
-            $http.post(url).then(function(response){
+            $http.post(url).then(function (response) {
+                vm.users = response.data;
+            });
+        }
+
+        /* NOT WORKING */
+        function addUser() {
+            console.log("Button clicked, script works");
+
+            var _name = document.getElementById("_name");
+            console.log("Input name: " + _name);
+            var _surname = document.getElementById("_name");
+            console.log("Input surname: " + _surname);
+            var _birthDay = document.getElementById("_birthDay");
+            console.log("Input birthDay: " + _birthDay);
+            var _login = document.getElementById("_login");
+            console.log("Input login: " + _login);
+
+            var User = {
+                name: _name,
+                surname: _surname,
+                birthDay: _birthDay,
+                login: _login
+            };
+
+            var url = "/users/create";
+            $http.post(url).then(function (response) {
                 vm.users = response.data;
             });
         }
     }
+
 })();
